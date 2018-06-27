1e0a57e923db2c8c2c2030ff21ffe948b4b2fcbd
enmalafeev <malafeev.en@yandex.ru>
2018-06-27 09:03:44 +0300

    finish parallax on index.html

--- src/assets/scripts/modules/parallax.js
+++ src/assets/scripts/modules/parallax.js
@@ -2,10 +2,11 @@ const parallaxContainer = document.querySelector(".parallax");
 const layers = Array.from(parallaxContainer.children);
 
 const moveLayers = e => {
-  const initialX = (window.innerWidth)/ 2 - e.pageX;
-  const initialY = (window.innerHeight) / 2 - e.pageY;
+  const initialX = window.innerWidth / 2 - e.pageX;
+  const initialY = window.innerHeight / 2 - e.pageY;
 
   layers.forEach((layer, i) => {
+    if (window.innerWidth <= 768) return;
     const divider = i / 100;
     const positionX = initialX * divider;
     const positionY = initialY * divider;
