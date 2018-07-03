3a63229ccb735f19ac386c6e56dd6b8b00de7d28
enmalafeev <malafeev.en@yandex.ru>
2018-07-03 23:11:27 +0300

    add axios and layout app.vue + reg_url

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,13 +1,51 @@
 <template lang="pug">
-  app-title
+  div#app
+    header.header
+      h1.header__title
+      a(href="index.html") Вернуться на сайт
+    nav.tab-menu
+      a(v-for="tab in 4" :value="tab" href="#") {{tab}}
+    main.content
+      h2.page-title Страница обо мне
+      div.skills
+        div(v-for="skill in 4" :value="skill").skill
+          p.skill__title
+          label.skill__name
+            input(type="number").skill__input
+          span.skill__procent %
+      button.btn Сохранить
+
+
+
 </template>
 
 <script>
+import inner from "./components/inner";
 export default {
   components: {
-    appTitle: require("./components/title").default
+    appTitle: require("./components/title").default,
+    inner
+  },
+  methods: {
+    handleClick(e) {
+      console.log("handleClick", e.target);
+    }
   }
 };
 </script>
-<style>
+<style lang="scss" scoped>
+.title {
+  border-bottom: 1px solid #000;
+  color: green;
+}
+
+.desc {
+  color: blue;
+  font-size: 24px;
+}
+
+.btn {
+  background-color: $green;
+  color: #fff;
+}
 </style>
