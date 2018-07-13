fc15eddd002e42bcedfe0c39fb2fbd1332bb601c
enmalafeev <malafeev.en@yandex.ru>
2018-07-13 17:50:12 +0300

    add cors-herokuapp link in request.js

--- src/admin/requests.js
+++ src/admin/requests.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://webdev-api.loftschool.com/";
+axios.defaults.baseURL =
+  "https://cors-anywhere.herokuapp.com/http://webdev-api.loftschool.com/";
 axios.defaults.headers["Authorization"] = `Bearer ${localStorage.getItem(
   "token"
 )}`;
