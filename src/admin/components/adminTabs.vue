daf1596e048722ff182535f21a79c0093b8a7317
enmalafeev <malafeev.en@yandex.ru>
2018-07-14 21:24:51 +0300

    change paths in adminTabs.vue for ghpages

--- src/admin/components/adminTabs.vue
+++ src/admin/components/adminTabs.vue
@@ -16,9 +16,9 @@ export default {
   data() {
     return {
       tabs: [
-        { title: "Обо мне", href: "/" },
-        { title: "Работы", href: "/works" },
-        { title: "Блог", href: "/blog" }
+        { title: "Обо мне", href: "/portfolio/dist/admin" },
+        { title: "Работы", href: "/portfolio/dist/works" },
+        { title: "Блог", href: "/portfolio/dist/blog" }
       ]
     };
   }
