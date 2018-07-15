1c13e5630f5c4d60a67a3ab8a2b93bd33b47f2cd
enmalafeev <malafeev.en@yandex.ru>
2018-07-15 23:20:13 +0300

    add smooth scroll on blog.html

--- src/assets/scripts/blog.js
+++ src/assets/scripts/blog.js
@@ -2,3 +2,4 @@ import "./modules/swipe-aside";
 import "./modules/popup";
 import "./modules/hamburger";
 import "./modules/blog-scroll";
+import "./modules/scroll-to-blog";
---
+++ src/assets/scripts/modules/scroll-to-blog.js
@@ -0,0 +1,8 @@
+const scrollToElement = require("scroll-to-element");
+
+const btnDown = document.querySelector("#btn-down");
+
+btnDown.addEventListener("click", e => {
+  e.preventDefault();
+  scrollToElement(".blog-main");
+});
