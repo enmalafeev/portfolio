8bb55f9eaa8cc96ac4a3415381e3fb1c23055590
enmalafeev <malafeev.en@yandex.ru>
2019-07-26 12:14:31 +0300

    refactoring hamburger.js

--- src/assets/scripts/modules/hamburger.js
+++ src/assets/scripts/modules/hamburger.js
@@ -1,11 +1,13 @@
 const strip1 = document.querySelector(".header__menu-strip-1");
 const strip2 = document.querySelector(".header__menu-strip-2");
 const strip3 = document.querySelector(".header__menu-strip-3");
+const stripArr = [strip1, strip2, strip3];
 
 const menu = document.querySelector(".hamburger");
 
 menu.addEventListener("click", e => {
-  strip1.classList.toggle("open");
-  strip2.classList.toggle("open");
-  strip3.classList.toggle("open");
+  stripArr.forEach(strip => strip.classList.toggle("open"));
+  // strip1.classList.toggle("open");
+  // strip2.classList.toggle("open");
+  // strip3.classList.toggle("open");
 });
