1befbe29771b4d005a557cefe2df1c0ca33fe123
enmalafeev <malafeev.en@yandex.ru>
2019-07-30 11:24:13 +0300

    add categories to vuex store

--- src/admin/store/index.js
+++ src/admin/store/index.js
@@ -5,10 +5,12 @@ Vue.use(Vuex);
 
 import skills from "./modules/skills";
 import works from "./modules/works";
+import categories from "./modules/categories";
 
 export const store = new Vuex.Store({
   modules: {
     skills,
-    works
+    works,
+    categories
   }
 });
